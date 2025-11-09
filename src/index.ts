4d8d90c673dbb648272a3c03701e46de000be2b9
MilagrosChavez <miluchavez2005@gmail.com>
2025-11-09 19:01:44 -0300

    fix cors error for new config in front

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use(
   session({

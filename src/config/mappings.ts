a2d73331e7141133bb1019b39aeaa4f1aa128493
ClarisaR <clarisacandia0108@gmail.com>
2025-11-10 18:32:29 -0300

    fix: mapping emotions and dream types

--- src/config/mappings.ts
+++ src/config/mappings.ts
@@ -10,10 +10,21 @@ export const stateMap: Record<string, string> = {
 };
 
 export const emotionMap: Record<string, string> = {
-  Felicidad: "9d45592d-247b-4778-afde-daca2002693c",
+  Alegria: "9d45592d-247b-4778-afde-daca2002693c",
   Tristeza: "28ef5090-cba6-4e56-8c96-cb8091581dcc",
   Miedo: "968e4082-b886-47cc-a19f-bd934de66d4a",
   Enojo: "889e4b20-3e43-4b4a-b6f0-22733ca0f43d",
+  Paz: "001b51da-a41b-4218-9b1e-c6f59544a097",
+  Frustracion: "133426d9-1331-40bf-b027-71f168407ed4",
+  Esperanza: "5020e38f-8666-4152-83c9-35271c2d0285",
+  Verguenza: "6844143c-a0e2-49fa-85df-c6dfe96e50c7",
+  Sorpresa: "9219b91b-aeb8-439b-b0b5-035a6ff2c5c4",
+  Celos: "9d6a4d1f-1035-4504-b53f-e6e51a4d56fa",
+  Nostalgia: "ac2cf457-4a08-4ed5-bc8c-aaecec2bd1d5",
+  Amor: "c022bff7-06cd-46c7-aeae-ae43995951e1",
+  Confusion: "ce03333c-a372-415e-a97d-404071bb828af",
+  Orgullo: "cfcb12a3-071a-4719-bfd2-ce254a328c6a",
+  Gratitud: "db7b37ba-8487-454f-9b1f-a4d4baba1d37"
 };
 
 export const dreamTypeMap: Record<string, string> = {
@@ -21,4 +32,6 @@ export const dreamTypeMap: Record<string, string> = {
   Pesadilla: "3ef3b451-9a82-4cab-9fd4-ca6f9769e3f4",
   Recurrente: "0531e25e-4b15-46f6-b02b-43cd66020e30",
   Estandar: "e2d8f9d0-0d03-4ea3-82b1-0bbb85b098c3",
+  Premonitorio: "56b55eed-add4-42ad-b54f-971f64d0cf99",
+  Vivido: "13063e93-931c-4309-bf0a-322a14744138"
 };

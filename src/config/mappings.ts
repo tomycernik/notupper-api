105a86f23f579c15836fc72f1173558693fac3f9
ClarisaR <clarisacandia0108@gmail.com>
2025-11-14 00:08:11 -0300

    fix: emotion map

--- src/config/mappings.ts
+++ src/config/mappings.ts
@@ -14,17 +14,12 @@ export const emotionMap: Record<string, string> = {
   Tristeza: "28ef5090-cba6-4e56-8c96-cb8091581dcc",
   Miedo: "968e4082-b886-47cc-a19f-bd934de66d4a",
   Enojo: "889e4b20-3e43-4b4a-b6f0-22733ca0f43d",
-  Paz: "001b51da-a41b-4218-9b1e-c6f59544a097",
   Frustracion: "133426d9-1331-40bf-b027-71f168407ed4",
-  Esperanza: "5020e38f-8666-4152-83c9-35271c2d0285",
   Verguenza: "6844143c-a0e2-49fa-85df-c6dfe96e50c7",
   Sorpresa: "9219b91b-aeb8-439b-b0b5-035a6ff2c5c4",
   Celos: "9d6a4d1f-1035-4504-b53f-e6e51a4d56fa",
   Nostalgia: "ac2cf457-4a08-4ed5-bc8c-aaecec2bd1d5",
-  Amor: "c022bff7-06cd-46c7-aeae-ae43995951e1",
   Confusion: "ce03333c-a372-415e-a97d-404071bb828a",
-  Orgullo: "cfcb12a3-071a-4719-bfd2-ce254a328c6a",
-  Gratitud: "db7b37ba-8487-454f-9b1f-a4d4baba1d37"
 };
 
 export const dreamTypeMap: Record<string, string> = {

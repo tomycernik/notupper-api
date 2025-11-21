05ce4c386b3d6b6c476089cca1d6faa1db59d28f
maertin <mutuverria00@gmail.com>
2025-11-21 20:39:05 -0300

    fix response headers

--- src/infrastructure/providers/illustration-skybox.provider.ts
+++ src/infrastructure/providers/illustration-skybox.provider.ts
@@ -1,25 +1,23 @@
 import { IllustrationProvider } from "@domain/providers/illustration.provider";
 import { envs } from "@config/envs";
-import { BlockadeLabsSdk } from '@blockadelabs/sdk';
+import { BlockadeLabsSdk } from "@blockadelabs/sdk";
 
 export class IllustrationSkyboxProvider implements IllustrationProvider {
+  private client: BlockadeLabsSdk;
 
-private client: BlockadeLabsSdk;
-
- constructor() {
+  constructor() {
     this.client = new BlockadeLabsSdk({
       api_key: envs.SKYBOX_API_KEY,
     });
   }
 
- async generateIllustration(dreamText: string): Promise<Buffer> {
-
+  async generateIllustration(dreamText: string): Promise<Buffer> {
     const request = {
       prompt: dreamText,
-      skybox_style_id: 11, //DreamLike style
+      skybox_style_id: 11, // DreamLike style
     };
 
-      const createResponse = await this.client.generateSkybox(request);
+    const createResponse = await this.client.generateSkybox(request);
 
     const taskId = createResponse.id;
     if (!taskId) {
@@ -27,17 +25,26 @@ private client: BlockadeLabsSdk;
     }
 
     let resultUrl: string | undefined;
+
     while (!resultUrl) {
       const status = await this.client.getImagineById({ id: taskId });
+
       if (status.status === "complete" && status.file_url) {
         resultUrl = status.file_url;
         break;
       }
+
       await new Promise((r) => setTimeout(r, 2000));
     }
 
-    const response = await fetch(resultUrl!);
+    const response = await fetch(resultUrl!, {
+      redirect: "follow",
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+      },
+    });
+
     const arrayBuffer = await response.arrayBuffer();
     return Buffer.from(arrayBuffer);
- }
+  }
 }
